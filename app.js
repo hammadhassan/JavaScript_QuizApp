84df23e8c5f0d665eb24bdd0b718bd35accfd539
Hammad Hassan <hammadhassansheikh@gmail.com>
2017-01-18 02:12:25 +0500

    apply javascript

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-alert("welcome");
+alert("Welcome to the Quiz of Pakistan Super League 2016");
+var q1 = document.getElementById("q1");
+var q2 = document.getElementById("q2");
+var q3 = document.getElementById("q3");
+var q4 = document.getElementById("q4");
+var q5 = document.getElementById("q5");
+var q6 = document.getElementById("q6");
+var q7 = document.getElementById("q7");
+var q8 = document.getElementById("q8");
+var q9 = document.getElementById("q9");
+var q10 = document.getElementById("q10");
+var q11 = document.getElementById("q11");
+var q12 = document.getElementById("q12");
