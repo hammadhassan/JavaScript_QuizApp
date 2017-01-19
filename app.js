f704c11e413bd739e5eb646ce7ddf815128e6fbb
Hammad Hassan <hammadhassansheikh@gmail.com>
2017-01-20 01:26:19 +0500

    hidden class

--- app.js
+++ app.js
@@ -12,17 +12,41 @@ var q9 = document.getElementById("q9");
 var q10 = document.getElementById("q10");
 var q11 = document.getElementById("q11");
 var q12 = document.getElementById("q12");
-
-var currentQuestion = 0,
-    score = 0,
-    askingQuestion = true;
 */
+var score = 0;
+var div = document.getElementsByTagName("p");
+var next = document.getElementsByClassName("cont");
+
+// begin_contain.className += " hidden";
+//     ques_cls[0].className = "show";
 
 function ques(ans) {
     if (ans =='correct') {
     alert("Correct Answer");
+    score++;
+    next[0].className = "show";
+    for (var i = 0; i = next.length; i++) {
+    div.className += " hidden";
+ };
     }
     else {
-    alert("Wrong Answer");   
+    alert("Wrong Answer");
+    // document.getElementByClassName("cont").className += "display";   
+    next[0].className = "show";
+    // div.previousSibling.style.visibility = "hidden";
+    for (var i = 0; i < next.length; i++) {
+    div.className += " hidden";
+ };
     }
  }; 
+if (score <= 1) {
+ function result(total) {
+    var num = (score * (12/100));
+    var resultShow = (num + "%") ;
+    span.innerHTML = score;
+    span.innerHTML = per + '%';
+    alert(resultShow);
+    }
+ };
+
+ 
