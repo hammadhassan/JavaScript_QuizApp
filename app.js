b4abb49511ea0a55519a267193265609773614c8
Hammad Hassan <hammadhassansheikh@gmail.com>
2017-01-21 20:26:43 +0500

    complete app without hidding

--- app.js
+++ app.js
@@ -1,25 +1,31 @@
 var score = 0;
 var next = document.getElementsByClassName("cont");
 
+var div = document.getElementsByTagName("div");
+//var classes = document.getElementsByClassName("");
+
 function ques(ans) {
     if (ans =="correct") {
-    alert("Correct Answer");
+    //alert("Correct Answer");
     score++;
+    //div[1].className = "hidden";
+    // for (var i = 1; i < div.length; i = i+2) {
+    // div[i].className = "hidden";
+    // };
     next[0].className = "show";
-    //next = next.className += " hidden";
     }
     else {
-    alert("Wrong Answer");
+    // alert("Wrong Answer");
     next[0].className = "show";
-    //next = next.className += " hidden";
+    // div[1].className = "hidden";
     }
  }; 
 
 //var span = document.getElementsByTagName("span");
 function result() {
     var num = ((score * (8.3/100)) * 100);
-    var res = Math.round(num);
-    document.getElementByTagName("span").innerHTML = ("<b>Your Result</b>" + res + "%");
+    var res = Math.ceil(num);
+    document.getElementById("result").innerHTML = ("<b>Your Score is </b>" + res + "%");
     alert(res);
     };
 
