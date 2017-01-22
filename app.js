670e4362339da925d71ec3d1b2aadfc33c861c28
Hammad Hassan <hammadhassansheikh@gmail.com>
2017-01-23 01:01:19 +0500

    add timer

--- app.js
+++ app.js
@@ -1,29 +1,50 @@
 var score = 0;
+var question = 0;
 var next = document.getElementsByClassName("cont");
 var div = document.getElementsByTagName("div");
 var show = document.getElementsByClassName("show");
+var li = document.getElementById("answer");
+var sec = 0;
+var min = 0;
+var t;
+var quest = document.getElementById("quesnum");
+var startbutton = document.getElementById("startbutton");
+//start Button
+function start() {
+    startbutton.className += " hidden";
+    next[0].className = "show";
+    show.className = "show";
+    t = setInterval(time, 1100);
+};
+//time
+function time() {
+    if (sec >= 60) {
+        sec = 00;
+        min++;
+    };
+    document.getElementById("timer").innerHTML = (min + ":" + sec++ );
+};
+//Answer Validation
 function ques(ans) {
     if (ans =="correct") {
     //alert("Correct Answer");
     score++;
-    //div[1].className = "hidden";
-    // for (var i = 1; i < div.length; i = i+2) {
-    // div[i].className = "hidden";
-    // };
+    question++;
     next[0].className = "show";
-    //div.previousSibling.className = "hidden"++;
     }
     else {
+    question++;
     // alert("Wrong Answer");
     next[0].className = "show";
-    //div[1].className = "hidden";
     }
+    document.getElementById("quesnum").innerHTML = (question + "/12");
     var shown = show[0];
     shown.className = "hidden";
     shown.nextElementSibling.className = "show";
  }; 
-
+//Result
 function result() {
+    clearTimeout(t);
     var num = (score * 8.3333);
     var res = Math.ceil(num);
     document.getElementById("showresult").innerHTML = ("<b>Your Score is </b>" + res + "%");
@@ -34,4 +55,4 @@ function result() {
  var playagain = document.getElementById("play");
  playagain.onclick = function () {
     location.reload();
-}
+};
